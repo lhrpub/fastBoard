1fd795852b28042d0df141652f7a6f6c40259893
lhrpub <dlghkfuf@naver.com>
2023-10-04 00:14:40 +0900

    #6 - 스프링 부트 프로젝트 시작
    스프링 프로젝트 생성

---
+++ src/main/java/com/fast/projectboard/FastProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fast.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastProjectBoardApplication.class, args);
+    }
+
+}
