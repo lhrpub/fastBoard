5016d2531e9d4bc30d2043c212074effadaed95b
lhrpub <dlghkfuf@naver.com>
2023-10-05 22:50:12 +0900

    # 16 - 기본 뷰 작성

--- src/main/java/com/fast/projectboard/controller/ArticleController.java
+++ src/main/java/com/fast/projectboard/controller/ArticleController.java
@@ -1,10 +1,19 @@
 package com.fast.projectboard.controller;
 
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/article")
+@RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+    @GetMapping
+    public String articles(ModelMap map) {
+        map.addAttribute("articles", List.of());
+        return "articles/index";
+    }
 
 }
--- src/test/java/com/fast/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fast/projectboard/controller/ArticleControllerTest.java
@@ -27,7 +27,7 @@ class ArticleControllerTest {
     public void 게시글리스호출() throws Exception {
         mvc.perform(get("/articles"))
             .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.TEXT_HTML))
+            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
             .andExpect(view().name("articles/index"))
             .andExpect(model().attributeExists("articles"));
     }
@@ -38,7 +38,7 @@ class ArticleControllerTest {
 
         mvc.perform(get("/articles/1"))
             .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.TEXT_HTML))
+            .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
             .andExpect(view().name("articles/detail"))
             .andExpect(model().attributeExists("article"))
             .andExpect(model().attributeExists("articleComments"));
